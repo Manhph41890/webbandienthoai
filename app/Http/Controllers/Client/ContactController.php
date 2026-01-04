2887dc82fde627ef50949382c5e8d9ce3e1c6c68
ManhDev <manh49719@gmail.com>
2026-01-04 09:36:18 +0700

    toiuu

--- app/Http/Controllers/Client/ContactController.php
+++ app/Http/Controllers/Client/ContactController.php
@@ -12,13 +12,4 @@ class ContactController extends Controller
         return view('pages.contact');
     }
 
-    public function store(ContactRequest $request) 
-{
-    Contact::create($request->validated());
-    
-    return response()->json(['message' => 'Gửi yêu cầu thành công']);
-}
-
-// Lọc dữ liệu cực chuyên nghiệp
-$contacts = Contact::byService(ContactService::WARRANTY_SUPPORT)->pending()->get();
 }
